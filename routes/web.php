f69197c59af6a5e02b3a4b4e2b0ed82d37ef44ea
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-27 11:55:18 +0700

    Creating Admin Login System

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [HomeController::class, 'index']);
 
+Route::get('/redirects', [HomeController::class, 'redirects']);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
