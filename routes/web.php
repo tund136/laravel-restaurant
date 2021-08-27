db8fa69788416fb7620a80aea690e9bd03bf6243
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-27 11:02:14 +0700

    Install Composer Jetstream

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [HomeController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
